5657690be4e9636b01166977228c01d7033c815e
Samuel Daniel <soma.hanna555@gmail.com>
2025-08-30 02:34:21 -0400

    Initial Commit

---
+++ HITFIT/HITFITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HITFITApp.swift
+//  HITFIT
+//
+//  Created by Owner on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct HITFITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
